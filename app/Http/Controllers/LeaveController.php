92ef652e6ce5c4707a612dd5bd88d59eab27d464
Alger Makiputin <algerzxc@gmail.com>
2018-11-25 14:08:02 +0800

    dev

--- app/Http/Controllers/LeaveController.php
+++ app/Http/Controllers/LeaveController.php
@@ -22,6 +22,7 @@ class LeaveController extends Controller
 
 	public function index() {
 		$sy = getCurrentSchoolYear();
+		
 	 	$sy_start = Carbon::create($sy['start'][0], $sy['start'][1])->format('Y-m-d');
 	 	$sy_end = Carbon::create($sy['end'][0], $sy['end'][1])->format('Y-m-d');
 	 	$school_year = $sy['sy'];
