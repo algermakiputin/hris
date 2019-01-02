1548aae4f40550d1427535caabbc1cc14ea5918f
alger makiputin <algerzxc@gmail.com>
2019-01-02 12:18:40 +0800

    --allow-empty-message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,16 +4,12 @@
 
   <body class="login">
     <div>
-    
-
-   
-    
-      <section class="login_content col-md-3 mx-auto">
+      <section class="login_content col-sm-3 col-sm-6 col-sm-12">
         <form autocomplete="off" id="login" method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
             @csrf
           <h1>Login </h1>
            <input autocomplete="false" name="hidden" type="text" style="display:none;">
-          <div class="form-group text-left">
+          <div class="form-group">
             <input type="email" required="required" class="form-control" id="email" placeholder="email" required=""  name="email" />
             @if ($errors->has('email'))
                 <span class="invalid-feedback text-danger" role="alert">
@@ -21,7 +17,7 @@
                 </span>
             @endif
           </div>
-          <div class="form-group text-left">
+          <div class="form-group">
             <input type="password" required="required" class="form-control" id="password" placeholder="password" required="" name="password" />
             @if ($errors->has('password'))
                 <span class="invalid-feedback text-danger" role="alert">
@@ -48,10 +44,6 @@
           </div>
         </form>
       </section>
-    
-
-       
-   
     </div>
   </body>
  
